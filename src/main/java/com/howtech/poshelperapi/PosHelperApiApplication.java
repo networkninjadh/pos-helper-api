4b7bed44234cb3c415cba678a29cd30dfc386490
networkninjadh <networkninjadh@gmail.com>
2022-04-12 12:18:40 -0500

    initial commit

---
+++ src/main/java/com/howtech/poshelperapi/PosHelperApiApplication.java
@@ -0,0 +1,13 @@
+package com.howtech.poshelperapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosHelperApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosHelperApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/howtech/poshelperapi/PosHelperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howtech.poshelperapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosHelperApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
